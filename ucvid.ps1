a90cf3b049755fc52482f7680ba31b8ea7838f96
genericuser84 <justin.park@jfs.ohio.gov>
2024-09-16 15:36:16 -0400

    Update ucvid.ps1

--- ucvid.ps1
+++ ucvid.ps1
@@ -1,5 +1,5 @@
 # Be sure to use *full paths
-$urlShortcutPath = "$HOME\Desktop\UCVideos.url"
+$urlShortcutPath = C:\Users\Public\Desktop\UCVideos.url"
 $url = 'https://jfs.ohio.gov/job-services-and-unemployment/unemployment/for-unemployed-workers/resources/03-how-to-videos'
 $iconPath = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
 
