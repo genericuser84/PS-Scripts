7dd85259d2c9f475cfeea2be0b178fbf06ce7e40
genericuser84 <justin.park@jfs.ohio.gov>
2026-06-26 14:13:28 -0400

    Update omjbot.ps1
    updated url

--- omjbot.ps1
+++ omjbot.ps1
@@ -1,6 +1,6 @@
 # Be sure to use *full paths
 $urlShortcutPath = "C:\users\public\Desktop\ChatBot.url"
-$url = 'https://jfs.ohio.gov/job-services-and-unemployment/unemployment?omj_office=true'
+$url = 'https://jfs.ohio.gov/unemployment-services?omj_office=true'
 $iconPath = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
 
 # Create the shortcut
